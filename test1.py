af3bc57a695094cd7f54ad41cda5ca1d006aeea6
papavask <44700690+papavask@users.noreply.github.com>
2025-01-17 13:43:19 +0200

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,10 +4,10 @@ from PIL import Image
 # URL or local path to the image
 image_url = "assets/bg2.jpg"  # Or use a local path like "assets/background.jpg"
 mime_type = image_url.split('.')[-1:][0].lower()
-   with open(image_url, "rb") as f:
-      content_bytes = f.read()
-   content_b64encoded = base64.b64encode(content_bytes).decode()
-   image_url = f'data:image/{mime_type};base64,{content_b64encoded}'
+with open(image_url, "rb") as f:
+   content_bytes = f.read()
+content_b64encoded = base64.b64encode(content_bytes).decode()
+image_url = f'data:image/{mime_type};base64,{content_b64encoded}'
 # Add the custom CSS to set the background image
 st.markdown(
     f"""
